4554e6394acf44f1ebbee474b6f7d12ce3da1054
M-Orxan <orxanm385@gmail.com>
2025-02-28 23:38:45 +0400

    TwoSum LeetCode question added

--- AlgorithmicQuestions/Program.cs
+++ AlgorithmicQuestions/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AlgorithmicQuestions;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+    }
+}
---
+++ AlgorithmicQuestions/TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicQuestions
+{
+    /// <summary>
+    /// Question Link: https://leetcode.com/problems/two-sum/description/
+    /// </summary>
+    public class TwoSum
+    {
+        public int[] Solve(int[] nums, int target)
+        {
+            Dictionary<int, int> numMaps = new Dictionary<int, int>();
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+
+                if (numMaps.ContainsKey(complement))
+                {
+                    return new int[] { numMaps[complement], i };
+                }
+                numMaps[nums[i]] = i;
+
+            }
+
+            return new int[0];
+
+        }
+    }
+}
