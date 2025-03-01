dcdba654a69f9986c69c8a47b9aa7ffcf4fb3cd1
M-Orxan <orxanm385@gmail.com>
2025-03-01 19:06:01 +0400

    Valid Parentheses Leetcode question added

---
+++ AlgorithmicQuestions/ValidParentheses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmicQuestions
+{
+    /// <summary>
+    /// Question Link: https://leetcode.com/problems/valid-parentheses/description/
+    /// </summary>
+    public class ValidParentheses
+    {
+        public bool IsValid(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '[' | input[i] == '(' || input[i] == '{')
+                    stack.Push(input[i]);
+
+                if (stack.Count == 0)
+                    return false;
+
+
+                if (input[i] == ']' && stack.Pop() != '[' ||
+                   input[i] == ')' && stack.Pop() != '(' ||
+                   input[i] == '}' && stack.Pop() != '{')
+                    return false;
+
+
+
+            }
+
+            return stack.Count == 0;
+
+        }
+
+
+    }
+}
